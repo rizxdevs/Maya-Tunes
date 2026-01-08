a1d68ce040e1d36ffc7e204b0286bdbb1b349124
Shibin Hussain MK <160138800+SHIBINSOJU@users.noreply.github.com>
2026-01-08 18:24:38 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,34 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { queryClient } from "./lib/queryClient";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
+import { Toaster } from "@/components/ui/toaster";
+
+import ProtectedRoute from "@/components/ProtectedRoute";
+
 import Landing from "@/pages/Landing";
 import Dashboard from "@/pages/Dashboard";
 import ServerList from "@/pages/ServerList";
 import ServerView from "@/pages/ServerView";
+import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
+      {/* Public */}
       <Route path="/" component={Landing} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/servers" component={ServerList} />
-      <Route path="/servers/:id" component={ServerView} />
+
+      {/* Protected */}
+      <ProtectedRoute path="/dashboard" component={Dashboard} />
+      <ProtectedRoute path="/servers" component={ServerList} />
+      <ProtectedRoute path="/servers/:id" component={ServerView} />
+
       <Route component={NotFound} />
     </Switch>
   );
 }
 
-function App() {
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
@@ -31,5 +38,3 @@ function App() {
     </QueryClientProvider>
   );
 }
-
-export default App;
